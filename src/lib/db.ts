3b79495f507303b6c9e8936d52bfafcd92b178ab
Gabriel Brito <gabrielbrito.contato2017@gmail.com>
2026-08-11 16:21:06 -0300

    ajuste no pool

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,21 @@
 import mysql from "mysql2/promise";
 
-export async function query(sql: string, values?: (string | number | boolean | null)[]): Promise<unknown> {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // máximo de conexões simultâneas
+  queueLimit: 0, // fila infinita (0 = sem limite)
+});
 
-  const [rows] = await connection.execute(sql, values);
-  connection.end();
+// Mantemos a função query para executar consultas SQL de forma assíncrona, retornando os resultados.
+export async function query(
+  sql: string,
+  values?: (string | number | boolean | null)[],
+): Promise<unknown> {
+  const [rows] = await pool.execute(sql, values);
   return rows;
 }
